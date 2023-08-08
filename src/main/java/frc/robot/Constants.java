9730cbe9f6c3b10b880dda4a91d50553e630ba66
Evan-Ko <evanytko@gmail.com>
2023-08-08 11:15:05 -0700

    Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,68 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  // joystick channels
+  public static final int RIGHT_JOYSTICK_CHANNEL = 2;
+  public static final int LEFT_JOYSTICK_CHANNEL = 0;
+
+  // arm angle positions
+  public static final int HOME_ARM_ANGLE = 40; 
+  public static final int TRAVELING_ARM_ANGLE_NOT_BLOCKING_CHASSIS_CAM = 295;
+  public static final int TOP_SCORING_ANGLE = 207;
+  public static final int TOP_TRANSITION_ANGLE = 210;
+  public static final int MID_SCORING_ANGLE = 233;
+  public static final int BOTTOM_SCORING_ANGLE = 326;
+  public static final int SUBSTATION_ANGLE = 279;
+  public static final boolean HOME_EXTEND = false;
+  public static final boolean TRAVELING_ARM_EXTEND = false;
+  public static final boolean TOP_SCORING_EXTEND = true;
+  public static final boolean MID_SCORING_EXTEND = true;
+  public static final boolean BOTTOM_SCORING_EXTEND = true;
+  public static final boolean PICKING_UP_EXTEND = true;
+  public static final boolean SUBSTATION_EXTEND = false;
+  // arm motor constants
+  public static final int ARM_EXTENDED_ALPHA = 116;
+  public static final int ARM_EXTENDED_LOWER_LIMIT = 50;
+  public static final int ARM_EXTENDED_UPPER_LIMIT = 360;
+
+  public static final int ARM_RETRACTED_ALPHA = 80;
+  public static final int ARM_RETRACTED_LOWER_LIMIT = 25;
+  public static final int ARM_RETRACTED_UPPER_LIMIT = 335;
+
+  public static final double FB_UPPER_LIMIT_CLOSED = 0.2;
+  public static final double FB_LOWER_LIMIT_CLOSED = -0.2;
+  public static final double FB_UPPER_LIMIT_OPEN = 0.35;
+  public static final double FB_LOWER_LIMIT_OPEN = -0.35;
+
+  public static final double ARM_MOTOR_FF_GAIN = -0.15;
+  public static final double ARM_ENCODER_OFFSET = -349;
+
+  // hat constants 
+  public static final int HAT_JOYSTICK_TRIM_POSITION = RIGHT_JOYSTICK_CHANNEL;
+  public static final int HAT_JOYSTICK_TRIM_ROTATION_ARM = LEFT_JOYSTICK_CHANNEL;
+  public static final double HAT_POWER_MOVE = 0.05;
+  public static final double HAT_POWER_ROTATE = 0.2;
+  // Hat trim target speed is 15 degrees per second
+  // One time step is 0.02 seconds
+  // 0.3 degrees per time step is our target change when the hat is active
+  public static final double HAT_POSE_TARGET_PER_TIME_STEP = -0.3; // negative is raising the arm
+  public static final int HAT_POV_MOVE_LEFT = 270;
+  public static final int HAT_POV_MOVE_RIGHT = 90;
+  public static final int HAT_POV_MOVE_FORWARD = 0;
+  public static final int HAT_POV_MOVE_BACK = 180;
+  public static final int HAT_POV_ARM_UP = 0;
+  public static final int HAT_POV_ARM_DOWN = 180;
+  public static final int HAT_POV_ROTATE_LEFT = 270;
+  public static final int HAT_POV_ROTATE_RIGHT = 90;
+  // absolute enconder for top arm id
+  public static final int ARM_ENCODER_ID = 7;
+
+  // arm driving motor id
+  public static final int TALON_CHANNEL = 21;
+
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+  public static final int kDriverControllerPort = 0;
+
   }
+  
 }
---
+++ src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
@@ -0,0 +1,116 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class ArmMotorSubsystem extends SubsystemBase {
+
+  public TalonFX armMotor = new TalonFX(Constants.TALON_CHANNEL);
+  private  long loopCtr = 0;
+  private PIDController pid = new PIDController(0.012, 0.0, 0.001); //added a little bit of kd to damp out velocity
+  private  double theta;
+  private  double m_poseTarget = Constants.TOP_SCORING_ANGLE - 10;
+  private double fPO;
+  private ClawPneumaticSubsystem cns;
+  
+  /** Creates a new ArmMotorSubsystem. */
+  public ArmMotorSubsystem(ClawPneumaticSubsystem cns) {
+    
+    pid.setTolerance(10);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    loopCtr++;
+    double upperLimit, lowerLimit, alpha;
+
+    if (RobotContainer.m_armPneumaticSubsystem.getIsExtended()) {
+      alpha = Constants.ARM_EXTENDED_ALPHA;
+      lowerLimit = Constants.ARM_EXTENDED_LOWER_LIMIT;
+      upperLimit = Constants.ARM_EXTENDED_UPPER_LIMIT;
+    } else {
+      alpha = Constants.ARM_RETRACTED_ALPHA;
+      lowerLimit = Constants.ARM_RETRACTED_LOWER_LIMIT;
+      upperLimit = Constants.ARM_RETRACTED_UPPER_LIMIT;
+    }
+    // Arm pose trim override
+    Joystick hatJoystickTrimRotationArm = (Constants.HAT_JOYSTICK_TRIM_ROTATION_ARM == Constants.LEFT_JOYSTICK_CHANNEL)
+      ? RobotContainer.leftJoystick
+      : RobotContainer.rightJoystick;
+    if(hatJoystickTrimRotationArm.getPOV()==Constants.HAT_POV_ARM_UP){
+      m_poseTarget += Constants.HAT_POSE_TARGET_PER_TIME_STEP;
+    }
+    if(hatJoystickTrimRotationArm.getPOV()==Constants.HAT_POV_ARM_DOWN){
+      m_poseTarget += Constants.HAT_POSE_TARGET_PER_TIME_STEP*-1.0;
+    }
+    m_poseTarget = MathUtil.clamp(m_poseTarget, lowerLimit, upperLimit);
+
+    // manual control of the upper arm with z axis slider
+    // double val = -RobotContainer.rightJoystick.getRawAxis(3);
+    // double angle = (val+1.0)*180.0;
+    // m_poseTarget = MathUtil.clamp(angle, lowerLimit, upperLimit);
+
+
+    theta = 360.0 * (RobotContainer.encoder.getValue() - Constants.ARM_ENCODER_OFFSET) / 4096.0;
+    theta %= 360.0;
+    if (theta < 0){
+      theta += 360.0;
+    }
+    fPO = (theta + alpha - 90.0);
+    fPO %= 360.0;
+    if (fPO < 0){
+      fPO += 360.0;
+    }
+    final double gain = Constants.ARM_MOTOR_FF_GAIN;
+    double ffMotorPower = gain * Math.sin(Math.toRadians(fPO));
+
+    double lowerLimitFB = cns.getIsClosed() ? Constants.FB_LOWER_LIMIT_CLOSED : Constants.FB_LOWER_LIMIT_OPEN;
+    double upperLimitFB = cns.getIsClosed() ? Constants.FB_UPPER_LIMIT_CLOSED : Constants.FB_UPPER_LIMIT_OPEN;
+    double fbMotorPower = MathUtil.clamp(pid.calculate(theta, m_poseTarget), lowerLimitFB, upperLimitFB);
+
+    if (loopCtr % 50 == 0) {
+    //   System.out.println("theta=" + theta + "   Target = " + m_poseTarget);
+    //   System.out.print("FF Motor Power = " + ffMotorPower);
+    //   System.out.println("   FB Motor Power = " + fbMotorPower);
+    //   System.out.println("fPO = " + fPO + "   Theta = " + theta + "   Alpha = " + alpha + "   Raw = " + RobotContainer.encoder.getValue());
+    }
+    double motorPower = fbMotorPower - ffMotorPower;
+    armMotor.set(ControlMode.PercentOutput, motorPower);
+    // putToSDB();
+  }
+
+  public void putToSDB() {
+    SmartDashboard.putNumber("theta", theta);
+    SmartDashboard.putNumber("fPO", fPO);
+    SmartDashboard.putNumber("Raw", RobotContainer.encoder.getValue());
+    SmartDashboard.putNumber("pneumatic state", (RobotContainer.m_armPneumaticSubsystem.getIsExtended() == true ? 1 : 0));
+  }
+
+  public double getTheta() { // arm angle with respect to the lower arm
+    return theta;
+  }
+
+  public void setPose(double poseTarget) {
+    m_poseTarget = poseTarget;
+    SmartDashboard.putNumber("Pose Target", poseTarget);
+  }
+
+  public boolean areWeThereYet() {
+    double sp = pid.getSetpoint();
+    boolean atSP = pid.atSetpoint();
+    return sp == 0.0 || atSP;
+  }
+ }
